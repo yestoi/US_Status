927e27e7a61b99b14d2007e12858f9c08565b71b
yestoi <yestoi1@gmail.com>
2009-10-05 16:48:08 -0400

    formatting

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ int main(int argc, char **argv)
     char *data;
 
     data = getdata("http://undergroundsystems.org");
+
     //Do regex and notify code here
     printf("%s", data);
-
     //END
 
     if (data)
